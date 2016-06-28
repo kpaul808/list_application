6a4f0f44f695b2b43c6b254abc247fe07c16faf0
kpaul808 <kainoap808@gmail.com>
2016-06-28 11:59:49 -1000

    added text to repo

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
-var arr = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+var buttons = [
+		"../assets/redButton.png",
+		"../assets/orangeButton.png",
+		"../assets/yellowButton.svg",
+		"../assets/greenButton.png",
+		"../assets/blueButton.png", 
+		"../assets/violetButton.png"
+];
+
+var colors = [
+		"green",
+		"blue",
+		"purple",
+		"red",
+		"orange",
+		"yellow"
+];
 
 var	count = 0;
-
 var cycle = function(array) {
 	console.log(array[count]);
-	if (count > array.length){
+	if (count == array.length-1){
 		count = 0;
 	} else {
 		count++;
-	}
+	};
 };
 
-var list = function() {
-	cycle(arr);
+var button_switch = function(array){
+	document.getElementById('JOHNCENA').src = buttons[count];
+	cycle(buttons);
+	background();
+}
+
+var background = function(){
+	document.body.style.backgroundColor = colors[count];
 }
